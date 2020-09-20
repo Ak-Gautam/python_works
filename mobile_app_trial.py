72043e755bff0b0d271df08189adde72650006c0
Gautam <Gautam-flash@users.noreply.github.com>
2020-09-20 13:04:58 +0530

    New Method

--- mobile_app_trial.py
+++ mobile_app_trial.py
@@ -4,41 +4,23 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
+class MyGrid(Widget):
+    name = ObjectProperty(None)
+    age = ObjectProperty(None)
 
-class Myspace(GridLayout):
-    def __init__(self, **kwargs):
-        super(Myspace, self).__init__(**kwargs)
+    def btn(self):
+        print("Name: ", self.name.text, "\tAge: ", self.age.text)
+        self.name.text, self.age.text = "",""
 
-        self.cols=1
-
-        self.inside = GridLayout()
-        self.inside.cols = 2
-
-        self.inside.add_widget(Label(text="Name: "))
-        self.name = TextInput(multiline=False)
-        self.inside.add_widget(self.name)
-        self.inside.add_widget(Label(text="Age: "))
-        self.age = TextInput(multiline=False)
-        self.inside.add_widget(self.age)
-
-        self.add_widget(self.inside)
-
-        self.submit = Button(text="Submit", font_size=30)
-        self.submit.bind(on_press=self.press)
-        self.add_widget(self.submit)
-
-    def press(self, instance):
-        name = self.name.text
-        age = self.age.text
-        print("Name: ", name,"\t", "Age: ", age)
-        self.name.text = ""
-        self.age.text = ""
+      
 
 
 class MyApp(App):
     def build(self):
-        return Myspace()
+        return MyGrid()
         
         
 if __name__ == "__main__":
