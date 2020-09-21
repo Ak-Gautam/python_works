747c0355d735ab421837e94bacdbd91c7791f554
Gautam <Gautam-flash@users.noreply.github.com>
2020-09-21 13:12:47 +0530

    Touch/mouse input

--- mobile_app_trial.py
+++ mobile_app_trial.py
@@ -1,12 +1,29 @@
 import kivy
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+
+class Touch(Widget):
+
+    btn = ObjectProperty(None)
+    def on_touch_down(self, touch):
+        print("Mouse down", touch)
+        self.btn.opacity = 0.5
+
+    def on_touch_move(self, touch):
+        print("Mouse move", touch)
+
+    def on_touch_up(self, touch):
+        print("Mouse up", touch)
+        self.btn.opacity = 1
+
 
 
 
 class MyApp(App):
     def build(self):
-        return FloatLayout()
+        return Touch()
         
         
 if __name__ == "__main__":
