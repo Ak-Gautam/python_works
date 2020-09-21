da8a1d8644d05e1f8d5e4f9a3f2503b035c66083
Gautam <Gautam-flash@users.noreply.github.com>
2020-09-21 13:41:23 +0530

    Drawing shapes (rectangles, lines, etc)

--- mobile_app_trial.py
+++ mobile_app_trial.py
@@ -2,22 +2,27 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.graphics import *
 
 
 class Touch(Widget):
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)
 
-    btn = ObjectProperty(None)
+        with self.canvas:
+           # Color(0.1, 0.4, 0.5, 0.8, mode='rgba')
+           # Line(points=(20, 30, 600, 100, 400, 500))
+            Color(0.4, 0.5, 0.7, 1, mode='rgba')
+            self.rect = Rectangle(pos=(300,300), size=(60,60))
+
+
     def on_touch_down(self, touch):
+        self.rect.pos = touch.pos
         print("Mouse down", touch)
-        self.btn.opacity = 0.5
 
     def on_touch_move(self, touch):
+        self.rect.pos = touch.pos
         print("Mouse move", touch)
-
-    def on_touch_up(self, touch):
-        print("Mouse up", touch)
-        self.btn.opacity = 1
-
 
 
 
