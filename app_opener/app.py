7ba7e17a892d05aad4c81fb98268b5b312a0267e
Gautam <Gautam-flash@users.noreply.github.com>
2020-09-25 12:47:00 +0530

    The app is able tto open the executable file. It's working!!

--- app_opener/app.py
+++ app_opener/app.py
@@ -5,7 +5,35 @@ import os
 
 root = tk.Tk()
 
-canavas = tk.Canvas(root, height=700, width=700, bg="#263D22")
+apps = []
+
+def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+
+    apps.append(filename)
+
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app, bg = "gray")
+        label.pack()
 
 
+def runApps():
+    for app in apps:
+        os.startfile(app)
+
+canavas = tk.Canvas(root, height=500, width=700, bg="#264D62")
+canavas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42", command=addApp)
+openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42", command=runApps)
+runApps.pack()
 root.mainloop()
