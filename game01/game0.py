9e7f57a309fbfddc72a94551820549b2e22cec69
Akshay Gautam <akshaygautam010@gmail.com>
2021-04-28 10:42:43 +0530

    Simple playable game

--- game01/game0.py
+++ game01/game0.py
@@ -1,22 +1,64 @@
 import pygame
+import sys
+from random import randint
 
-pygame.init()
-screen = pygame.display.set_mode((600, 480))
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([20, 20])
+        self.image.fill((255, 40, 200))
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    fps = 50
+    bg = [0, 0, 0]
+    size = [400, 400]
+    screen = pygame.display.set_mode(size)
+    player = Sprite([40, 50])
+    player.move = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
+    player.vx = 5
+    player.vy = 5
+
+    wall = Sprite([randint(0, 400), randint(1, 390)])
+
+    wall_group = pygame.sprite.Group()
+    wall_group.add(wall)
+
+    player_group = pygame.sprite.Group()
+    player_group.add(player)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+
+        key = pygame.key.get_pressed()
+        for i in range(2):
+            if key[player.move[i]]:
+                player.rect.x += player.vx*[-1, 1][i]
+
+        for i in range(2):
+            if key[player.move[2:4][i]]:
+                player.rect.y += player.vy*[-1, 1][i]
+
+        screen.fill(bg)
+
+        hit = pygame.sprite.spritecollide(player, wall_group, True)
+
+        if hit:
+            player.image.fill((255, 255, 255))
+
+        player_group.draw(screen)
+        wall_group.draw(screen)
+        pygame.display.update()
+        clock.tick(fps)
 
 
-done = False
+    pygame.quit()
+    sys.exit
 
-font = pygame.font.SysFont("Times new Roman", 62)
-
-text = font.render("Hello, Pygame", True, (150, 50, 50))
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            done = True
-    screen.fill((255, 255, 255))
-
-    screen.blit(text, (300 - text.get_width()//2, 240-text.get_height()//2))
-
-    pygame.display.flip()
+if __name__ == '__main__':
+    main()
